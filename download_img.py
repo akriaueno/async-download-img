a26764d53c0f47160a3f9ced3b9d21cc3461d0a6
akriaueno <akiraueno@outlook.com>
2019-09-21 00:40:51 +0900

    change not use run_in_executor

--- download_img.py
+++ download_img.py
@@ -5,16 +5,33 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def timer(func):
+    if asyncio.iscoroutinefunction(func):
+        async def async_wrapper(*args, **kwargs):
+            start = time.time()
+            await func(*args, **kwargs)
+            elapsed_time = time.time() - start
+            print(f'{func.__name__}: {elapsed_time}[s]')
+        return async_wrapper
+    else:
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            func(*args, **kwargs)
+            elapsed_time = time.time() - start
+            print(f'{func.__name__}: {elapsed_time}[s]')
+        return wrapper
+
+
 async def async_download_imgs(urls, loop):
     try:
-        def async_download_img(url, loop):
+        async def async_download_img(url):
             r = requests.get(url)
             file_name = url.split('/')[-1]
             file_path = f'async_{file_name}'
             with open(file_path, 'wb') as f:
                 f.write(r.content)
-        tasks = [loop.run_in_executor(None, async_download_img, url, loop) for url in urls]
-        return await asyncio.gather(*tasks)
+        cors = [async_download_img(url) for url in urls]
+        return await asyncio.gather(*cors)
     except Exception:
         pass
 
@@ -30,6 +47,7 @@ def download_imgs(urls):
     except Exception:
         pass
 
+
 def get_img_list(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
@@ -43,19 +61,19 @@ def format_img_srcs(img_srcs, url):
     url = url[:-1] if url[-1] == '/' else url
     return [img_src if pattern.match(img_src) else f'{url}{img_src}' for img_src in img_srcs]
 
-            
+
 if __name__ == '__main__':
     img_list = get_img_list('https://news.google.com/')
     print(img_list)
     print(f'{len(img_list)} files will be downloaded')
     print('continue ? [Y/n]')
     if input() in ['Y', 'y']:
+        start = time.time()
         loop = asyncio.get_event_loop()
-        async_start = time.time()
         loop.run_until_complete(async_download_imgs(img_list, loop))
-        async_elapsed_time = time.time() - async_start
-        normal_start = time.time()
+        elapsed_time = time.time() - start
+        print(f'{async}: {elapsed_time}[s]')
+        start = time.time()
         download_imgs(img_list)
-        normal_elapsed_time = time.time() - normal_start
-        print(f'async: {async_elapsed_time}[s]')
-        print(f'normal: {normal_elapsed_time}[s]')
+        elapsed_time = time.time() - start
+        print(f'{normal}: {elapsed_time}[s]')
